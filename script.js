1409418f13c7c412dde51e3aa35ae4f910a9a674
Jason Gagne <jas.gagne@outlook.com>
2021-06-15 10:33:16 -0400

    Completed Fundamentals Part 1 questions.

---
+++ script.js
@@ -0,0 +1,19 @@
+/**
+ * * Fundamentals Part 1 
+ **/
+
+// 1. Add 2 numbers together!
+console.log(23 + 97);
+
+// 2. Add a sequence of 6 different numbers together.
+let sequence = (6 * 5) + (7 - 2) * (6 + 5);
+console.log(sequence);
+
+// 3. Print the solution to the following equation: (4 + 6 + 9) / 77
+let equation = (4 + 6 + 9) / 77;
+console.log(equation);
+
+/**
+ * * Fundamentals Part 2 
+ **/
+
